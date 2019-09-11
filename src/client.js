c3b87eecc2a9a1063f64703aa2fa2f8c0acc032a
alialfredji <alfredjiali@gmail.com>
2019-09-11 21:20:05 +0200

    ready

--- src/client.js
+++ src/client.js
@@ -2,14 +2,17 @@
 const endpoint = process.env.ROOT_URL
 const io_client = require('socket.io-client')(endpoint);
 
-io_client.on('disconnect', () => console.log('disconnect'))
+const shortSessionId = process.env.TERM_SESSION_ID.split(':')[0]
+const loggedInClientId = `[${shortSessionId}-${process.env.USER}]`
+
+io_client.on('disconnect', () => console.log('Socket Server got disconnected'))
 
 io_client.on('connect', () => {
-    io_client.emit('server_client_connected', { clientId: process.env.USER })
+    io_client.emit('client_connect', { clientId: loggedInClientId })
 
     process.stdin.resume();
     process.stdin.setEncoding('utf8');
-    process.stdout.write(`Hi ${process.env.USER}, this is a chat. Write a message and press ENTER:\n\n`)
+    process.stdout.write(`Hi ${process.env.USER}, this is a chat. Write a message and press ENTER:\n`)
 
     process.stdin.on('data', function (text) {
         if (text.trim() === 'exit()') {
@@ -18,17 +21,26 @@ io_client.on('connect', () => {
         }
 
         io_client.emit('text_message', {
-            clientId: process.env.USER,
+            clientId: loggedInClientId,
             text,
         })
     })
 })
 
 io_client.on('new_message', ({ clientId, text }) => {
-    process.stdout.write(`[${clientId}]: ${text}`)
+    process.stdout.write(`\n`)
+    process.stdout.write(`${clientId}: ${text}`)
 })
 
-io_client.on('new_user', ({ clientId, text }) => {
-    if (clientId === process.env.USER) return
-    process.stdout.write(`new user entered chat: [${clientId}]\n`)
+io_client.on('new_user', ({ clientId }) => {
+    if (clientId === loggedInClientId) return
+    process.stdout.write(`\n`)
+    process.stdout.write(`User entered chat: ${clientId}\n`)
+})
+
+io_client.on('remove_user', ({ clientId }) => {
+    if (clientId === loggedInClientId) return
+
+    process.stdout.write(`\n`)
+    process.stdout.write(`User exited chat: ${clientId}\n`)
 })
--- src/server.js
+++ src/server.js
@@ -7,9 +7,18 @@ const http = require('http').createServer(app)
 const linkedSocket = io(http)
 
 linkedSocket.on('connection', (socket) => {
-    socket.on('disconnect', (info) => console.log('user disconnected'))
-    socket.on('server_client_connected', (payload) => linkedSocket.emit('new_user', payload))
-    socket.on('text_message', (payload) => linkedSocket.emit('new_message', payload))
+    socket.on('disconnect', () => {
+        linkedSocket.emit('remove_user', { clientId: socket.id })
+    })
+
+    socket.on('client_connect', (payload) => {
+        socket.id = payload.clientId
+        linkedSocket.emit('new_user', payload)
+    })
+
+    socket.on('text_message', (payload) => {
+        linkedSocket.emit('new_message', payload)
+    })
 })
 
 http.listen(process.env.PORT || '3000', () => {
